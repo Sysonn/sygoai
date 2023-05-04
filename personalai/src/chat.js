8fa3c599f624914c9c37e5a2516d4f7479f5121a
Derek Sisson <sissonderek@icloud.com>
2023-05-04 02:57:33 -0400

    Axios fixes

--- personalai/src/chat.js
+++ personalai/src/chat.js
@@ -158,7 +158,7 @@ const Chat = () => {
     e.preventDefault();
     setLoading(true); // set loading to true when API call is made
     try {
-      const res = await axios.post('https://localhost:5000/chat', { input });
+      const res = await axios.post('http://localhost:5000/chat', { input });
       setResponses([...responses, {input, response: res.data.response}]);
     } catch (err) {
       console.error(err);
